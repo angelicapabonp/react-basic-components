6a039f54813287b7b404a7b5071b86c9556f0323
Angelica Pabón <angelica.pabon@bicevida.cl>
2021-03-20 17:43:50 -0300

    fix(alias): remove alias

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import * as FORMATERS from 'utils/formaters';
+import * as FORMATERS from './src/utils/formaters';
 
-export { default as Button } from 'components/Button';
-export { default as CircularLoader } from 'components/CircularLoader';
-export { default as Icon } from 'components/Icon';
-export { default as TextField } from 'components/TextField';
+export { default as Button } from './src/components/Button';
+export { default as CircularLoader } from './src/components/CircularLoader';
+export { default as Icon } from './src/components/Icon';
+export { default as TextField } from './src/components/TextField';
 export { FORMATERS };
